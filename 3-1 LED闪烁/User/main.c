93d3d78f4e1810752130661b119c2f21b10f8332
linzhenghao233 <1392872316@qq.com>
2024-01-10 20:55:02 +0800

    Initial commit

---
+++ 3-1 LED闪烁/User/main.c
@@ -0,0 +1,32 @@
+#include "stm32f10x.h"                  // Device header
+#include "Delay.h"
+
+int main(void){
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
+	
+	GPIO_InitTypeDef GPIO_InitStructure;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;//推挽输出
+	//GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_OD;//开漏输出
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(GPIOA, &GPIO_InitStructure);
+	
+	//GPIO_ResetBits(GPIOA, GPIO_Pin_0);//低电平
+	//GPIO_SetBits(GPIOA, GPIO_Pin_0);
+	//GPIO_WriteBit(GPIOA, GPIO_Pin_0, Bit_RESET);//低电平
+	GPIO_WriteBit(GPIOA, GPIO_Pin_0, Bit_SET);
+	
+	while(1){
+		/*
+		GPIO_WriteBit(GPIOA, GPIO_Pin_0, Bit_RESET);
+		Delay_ms(500);
+		GPIO_WriteBit(GPIOA, GPIO_Pin_0, Bit_SET);
+		Delay_ms(500);
+		*/
+		
+		GPIO_WriteBit(GPIOA, GPIO_Pin_0, (BitAction)0);
+		Delay_ms(500);
+		GPIO_WriteBit(GPIOA, GPIO_Pin_0, (BitAction)1);
+		Delay_ms(500);
+	}
+}
