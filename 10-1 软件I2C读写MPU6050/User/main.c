c1068827bca7cdb8fb57d11b6b997fc91cebe7ce
linzhenghao233 <1392872316@qq.com>
2024-01-25 21:42:20 +0800

    0125

--- 10-1 软件I2C读写MPU6050/User/main.c
+++ 10-1 软件I2C读写MPU6050/User/main.c
@@ -1,20 +1,23 @@
 #include "stm32f10x.h"                  // Device header
 #include "Delay.h"
 #include "OLED.h"
-#include "MyI2C.h"
+#include "MPU6050.h"
+
+int16_t AX, AY, AZ, GX, GY, GZ;
 
 int main(void){
 	OLED_Init();
-	MyI2C_Init();
+	MPU6050_Init();
 	
-	MyI2C_Start();
-	MyI2C_SendByte(0xD0);	//1101 000 0(0写1读), AD0接高电平时是1101 001 0
-	uint8_t Ack = MyI2C_ReceiveAck();
-	MyI2C_Stop();
-	
-	OLED_ShowNum(1, 1, Ack, 3);
-	
+	OLED_ShowString(1, 1, "ID:");
+	OLED_ShowHexNum(1, 4, MPU6050_GetID(), 2);
 	while(1){
-		
+		MPU6050_GetData(&AX, &AY, &AZ, &GX, &GY, &GZ);
+		OLED_ShowSignedNum(2, 1, AX, 5);
+		OLED_ShowSignedNum(3, 1, AY, 5);
+		OLED_ShowSignedNum(4, 1, AZ, 5);
+		OLED_ShowSignedNum(2, 8, GX, 5);
+		OLED_ShowSignedNum(3, 8, GY, 5);
+		OLED_ShowSignedNum(4, 8, GZ, 5);
 	}
 }
