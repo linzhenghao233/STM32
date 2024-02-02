ae3e34b7d8e7332fb97472c6670ab8e8bd9e1f38
linzhenghao233 <1392872316@qq.com>
2024-02-02 23:26:24 +0800

    0202

---
+++ 12-2 实时时钟/User/main.c
@@ -0,0 +1,18 @@
+#include "stm32f10x.h"                  // Device header
+#include "Delay.h"
+#include "OLED.h"
+#include "MyRTC.h"
+
+int main(void){
+	OLED_Init();
+	MyRTC_Init();
+	
+	OLED_ShowString(1, 1, "Date:XXXX-XX-XX");
+	OLED_ShowString(2, 1, "Time:XX:XX:XX");
+	OLED_ShowString(3, 1, "CNT:");
+	
+	while(1){
+		MyRTC_ReadTime();
+		OLED_ShowNum(1, 1, RTC_GetCounter(), 10);;
+	}
+}
